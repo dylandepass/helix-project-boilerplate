78ba4b67069c32b270294b77be2f89656a3ddd2d
Dylan Depass <dylandepass@gmail.com>
2023-03-17 13:26:15 -0400

    fix: run lighthouse on pr

---
+++ .lighthouserc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  ci: {
+    collect: {
+        startServerCommand: 'npx @adobe/helix-cli up --pages-url https://main--helix-project-boilerplate--adobe.hlx.page',
+        startServerReadyPattern: "Enabled reverse proxy",
+        url: ['http://127.0.0.1:3000']
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+    assert: {
+      assertions: {
+        'categories:performance': ['error', {minScore: 1}],
+      }
+    },
+  },
+};
