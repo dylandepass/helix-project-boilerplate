5d4598c6eb071d501f35cb759866d8cd4541a992
Dylan Depass <dylandepass@gmail.com>
2023-03-17 12:37:49 -0400

    fix: add startServerReadyPattern

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,6 +2,7 @@ module.exports = {
   ci: {
     collect: {
         startServerCommand: 'npx @adobe/helix-cli up --pages-url https://main--helix-project-boilerplate--adobe.hlx.page',
+        startServerReadyPattern: "Enabled reverse proxy",
         url: ['http://127.0.0.1:3000']
     },
     upload: {
